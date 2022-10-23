8683d2dbd838d5186f26bb1eed0a64102ce9b786
Antoine Voillot <58462517+Psykoxen@users.noreply.github.com>
2022-10-23 21:08:34 +0200

    Delete log.py

--- log.py
+++
@@ -1,4 +0,0 @@
-access_token = "1457877644184330248-DPix1oBkVjjTXuDb2O2mBhU1HCi4df"
-access_token_secret = "PT13xs5ArD1su451XEtTRiDZjGvvmZWbRWEKMLVGFVnSQ"
-consumer_key = "cu9iN6SdE94ORAaE9780lJGIu"
-consumer_secret = "djBvBfdtRj8dAxVkcegHdyRZj7DJIrF6KvVzaVme5ewiYqP7nx"
